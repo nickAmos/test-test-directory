dcaab90e67e1bcf90791d5b0c12d328f15771ef8
nickAmos <132029265+nickAmos@users.noreply.github.com>
2023-06-09 11:22:01 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+import './Header.css';
+import { GrTestDesktop } from 'react-icons/gr';
+
+
+export default function Header() {
+
+
+    return(
+        <header>
+            <div id='left-box'>
+                <h1>Pathology assistant</h1>
+            </div>
+
+            <div id='right-box'>
+            <a target='_blank' rel='noreferrer' href='https://www.austinpathology.org.au/test-directory'>
+        <GrTestDesktop  size={50}/>
+        </a>
+      
+            </div>
+        </header>
+    )
+}
+
