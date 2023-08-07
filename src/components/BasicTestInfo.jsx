b36c23459df7dfc2a26041a10ad3acc6e25430bd
nickAmos <132029265+nickAmos@users.noreply.github.com>
2023-08-07 11:34:48 +1000

    Small fixes
    Made search bar more readable. Fixed bug where reffered laboratory was a different font to the rest of the test infomation.

--- src/components/BasicTestInfo.jsx
+++ src/components/BasicTestInfo.jsx
@@ -9,7 +9,7 @@ export default function BasicTestInfo({specimen, container, testcode, testInfo, 
             <p id='info'><b>Specimen type:</b> <span id='variable' >{specimen}</span></p>
             <p id='info'><b>Container:</b> <span id='variable'>{container}</span></p>
             <p id='info'><b>CSR Test code:</b> <span id='variable' >{testcode}</span></p>
-            {(laboratory === 'Referred Test') ? <p><b>Reffered to:</b> {testInfo['External Laboratory']}</p> : <p id='info'><b>Testing department:</b> <span id='variable' > {laboratory}</span></p> }
+            {(laboratory === 'Referred Test') ? <p id='info'><b>Reffered to:</b> <span id='variable'>{testInfo['External Laboratory']}</span></p> : <p id='info'><b>Testing department:</b> <span id='variable' > {laboratory}</span></p> }
             {altNames ? <p id='info'><b>Alternate names:</b> <span id='variable'>{altNames}</span></p> : null }
         </div>
 
